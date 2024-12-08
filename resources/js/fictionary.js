5d02d155323bf7ac2064a6b6034c757b01a64f0c
Laura Tonning <tonning@gmail.com>
2024-12-08 15:22:34 +0100

    Clean up

--- resources/js/fictionary.js
+++ resources/js/fictionary.js
@@ -2,7 +2,7 @@
 // window.CountUp = CountUp
 import {CountUp} from "countup.js";
 window.CountUp = CountUp
-console.log(CountUp, window.CountUp)
+// console.log(CountUp, window.CountUp)
 // const countUp = new CountUp('targetId', 5234);
 // if (!countUp.error) {
 //     countUp.start();
